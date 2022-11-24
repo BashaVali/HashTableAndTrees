c0e7aa7b84c2928c7ce1ab8757421823bf91c01e
BashaVali <bashavali222@gmail.com>
2022-11-24 05:58:02 +0530

    HashTable And Trees

---
+++ HashTableDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome To Hash Table And Tree ");
+            Console.WriteLine("1.Inserion Binary Search Tree");
+            Console.WriteLine("Enter the option");
+            int num = Convert.ToInt32(Console.ReadLine());
+            BST<int> binarySearchTree = new BST<int>(56);
+            switch (num)
+            {
+                case 1:
+                    binarySearchTree.Insert(30);
+                    binarySearchTree.Insert(70);
+                    binarySearchTree.Display();
+                    break;
+                default:
+                    Console.WriteLine("Enter valid option!!!");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
